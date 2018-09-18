cf3a9a91cc69516b504305972aca057d1ec46b39
Anna Artobolevskaia <artobolevskaia.a@garagetools.ru>
2018-09-18 16:20:22 +0300

    Footer nav

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Nav from './Nav/Nav';
 
 export class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        Footer
+        <div className="footer-inner">
+          <div className="footer-col -nav">
+            <Nav />
+          </div>
+          <div className="footer-col -social" />
+        </div>
       </div>
     );
   }
