e518273fe540c5b629688afe3af006b001d59c4e
Anna Aloria <C:\Users\Aloria\AppData\Roaming\The Bat!>
2018-09-21 21:30:25 +0300

    Social

--- src/components/Footer/Nav/Nav.jsx
+++ src/components/Footer/Nav/Nav.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 export class Nav extends Component {
   render() {
 
@@ -8,21 +7,21 @@ export class Nav extends Component {
       <div className="footer-nav">
         <div className="footer-nav-inner">
           <nav className="footer-nav-items">
-            {/*<div className="footer-nav-item">*/}
-              {/*<a className="footer-nav-link" href="#">Custody Home</a>*/}
-            {/*</div>*/}
-            {/*<div className="footer-nav-item">*/}
-              {/*<a className="footer-nav-link" href="#">Read FAQ</a>*/}
-            {/*</div>*/}
-            {/*<div className="footer-nav-item">*/}
-              {/*<a className="footer-nav-link" href="#">Contact Support</a>*/}
-            {/*</div>*/}
-            {/*<div className="footer-nav-item">*/}
-              {/*<a className="footer-nav-link" href="#">Privacy Policy</a>*/}
-            {/*</div>*/}
-            {/*<div className="footer-nav-item">*/}
-              {/*<a className="footer-nav-link" href="#">Terms of use</a>*/}
-            {/*</div>*/}
+            <div className="footer-nav-item">
+              <a className="footer-nav-link" href="#">Custody Home</a>
+            </div>
+            <div className="footer-nav-item">
+              <a className="footer-nav-link" href="#">Read FAQ</a>
+            </div>
+            <div className="footer-nav-item">
+              <a className="footer-nav-link" href="#">Contact Support</a>
+            </div>
+            <div className="footer-nav-item">
+              <a className="footer-nav-link" href="#">Privacy Policy</a>
+            </div>
+            <div className="footer-nav-item">
+              <a className="footer-nav-link" href="#">Terms of use</a>
+            </div>
           </nav>
         </div>
       </div>
---
+++ src/components/Footer/Social/Social.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+
+export class Social extends Component {
+  render() {
+
+    return (
+      <div className="footer-social">
+        <div className="footer-social-inner">
+          <header className="footer-social-header">
+            <div className="footer-social-title">Follow us</div>
+          </header>
+
+          <div className="footer-social-items">
+            <div className="footer-social-item">
+              <a href="#" className="footer-social-link">
+                <span className="icon icon-facebook" />
+              </a>
+            </div>
+            <div className="footer-social-item">
+              <a href="#" className="footer-social-link">
+                <span className="icon icon-instagram" />
+              </a>
+            </div>
+            <div className="footer-social-item">
+              <a href="#" className="footer-social-link">
+                <span className="icon icon-linkedin" />
+              </a>
+            </div>
+            <div className="footer-social-item">
+              <a href="#" className="footer-social-link">
+                <span className="icon icon-twitter" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Social;
